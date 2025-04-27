3213465f7092d3eb42d21e1d1b08ce72d9b7ab0f
Maam Yeerim <admyasamb@gmail.com>
2025-04-27 13:07:22 -0400

    NEW COMMIT

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -32,7 +32,9 @@ def bad_request(error):
     app.logger.warning(message)
     return (
         jsonify(
-            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
+            status = status.HTTP_400_BAD_REQUEST
+            error = "Bad Request"
+            message = message
         ),
         status.HTTP_400_BAD_REQUEST,
     )
@@ -42,7 +44,9 @@ def bad_request(error):
 def not_found(error):
     """Handles resources not found with 404_NOT_FOUND"""
     message = str(error)
-    app.logger.warning(message)
+    app.logger.warning(
+        message
+    )
     return (
         jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
         status.HTTP_404_NOT_FOUND,
