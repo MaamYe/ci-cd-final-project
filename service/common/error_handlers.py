279cb9b86ee55e3f8082f4c484c4861e1d16e62f
Maam Yeerim <admyasamb@gmail.com>
2025-04-27 13:43:00 -0400

    NEW COMMIT READIBILITY ENHANCED V2

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -28,13 +28,13 @@ from . import status
 @app.errorhandler(status.HTTP_400_BAD_REQUEST)
 def bad_request(error):
     """Handles bad requests with 400_BAD_REQUEST"""
-    message = str(error)
+    message=str(error)
     app.logger.warning(message)
     return (
         jsonify(
-            status = status.HTTP_400_BAD_REQUEST,
-            error = "Bad Request",
-            message = message
+            status=status.HTTP_400_BAD_REQUEST,
+            error="Bad Request",
+            message=message
         ),
         status.HTTP_400_BAD_REQUEST,
     )
@@ -43,7 +43,7 @@ def bad_request(error):
 @app.errorhandler(status.HTTP_404_NOT_FOUND)
 def not_found(error):
     """Handles resources not found with 404_NOT_FOUND"""
-    message = str(error)
+    message=str(error)
     app.logger.warning(
         message
     )
@@ -60,7 +60,7 @@ def not_found(error):
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
 def method_not_supported(error):
     """Handles unsupported HTTP methods with 405_METHOD_NOT_SUPPORTED"""
-    message = str(error)
+    message=str(error)
     app.logger.warning(message)
     return (
         jsonify(
@@ -75,7 +75,7 @@ def method_not_supported(error):
 @app.errorhandler(status.HTTP_409_CONFLICT)
 def resource_conflict(error):
     """Handles resource conflicts with HTTP_409_CONFLICT"""
-    message = str(error)
+    message=str(error)
     app.logger.warning(message)
     return (
         jsonify(
@@ -90,7 +90,7 @@ def resource_conflict(error):
 @app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 def mediatype_not_supported(error):
     """Handles unsupported media requests with 415_UNSUPPORTED_MEDIA_TYPE"""
-    message = str(error)
+    message=str(error)
     app.logger.warning(message)
     return (
         jsonify(
@@ -105,7 +105,7 @@ def mediatype_not_supported(error):
 @app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
 def internal_server_error(error):
     """Handles unexpected server error with 500_SERVER_ERROR"""
-    message = str(error)
+    message=str(error)
     app.logger.error(message)
     return (
         jsonify(
